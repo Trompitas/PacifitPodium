5c3fab2a31e962b6824b5b9b0ba218038ea74a8c
Trompitas <90296185+Trompitas@users.noreply.github.com>
2022-06-03 21:52:43 -0500

    Add files via upload

---
+++ src/models/usuarioModels.js
@@ -0,0 +1,19 @@
+const {Schema, model} = require("mongoose");
+
+const usuarioSchema = Schema({
+  nombreCompleto: {
+    type: String,
+    required: true,
+  },
+  cantidadRepeticiones: {
+    type: Number,
+    requiered: true,
+  },
+},
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = model('Rank', usuarioSchema);
